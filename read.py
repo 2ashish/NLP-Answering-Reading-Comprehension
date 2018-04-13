e8f5b1ac663da7a954c8bba6d96df681bc5ca455
2ashish <mr.ashish.singh96@gmail.com>
2018-04-14 01:31:07 +0530

    read.py updated now reading test data also

--- read.py
+++ read.py
@@ -45,3 +45,32 @@ def get_data(dataset):
                 span_file.write('-' + '\n')
 
 get_data(squad_dataset)
+
+squad_dataset = json.load(open('SQuAD/dev.json'))
+
+def get_data_test(dataset):
+    with open(os.path.join('./data/', 'test_context'), 'w') as context_file,     open(os.path.join('./data/', 'test_question'), 'w') as question_file,     open(os.path.join('./data/', 'test_answer'), 'w') as answer_file,     open(os.path.join('./data/', 'test_span'), 'w') as span_file:
+        for data_id in range(len(dataset['data'])):
+            data = dataset['data'][data_id]
+            for para_id in range(len(data['paragraphs'])):
+                para = data['paragraphs'][para_id]
+                context = para['context']
+                if context[0] == '\n':
+                    context = context[-len(context)+1:]
+                qas = para['qas']
+                for qid in range(len(qas)):
+                    question = qas[qid]['question']
+                    answer_text = qas[qid]['answers'][0]['text']
+                    answer_start = qas[qid]['answers'][0]['answer_start']
+                    answer_end = answer_start + len(answer_text)
+                    
+                    question_file.write(question + '\n')
+                    answer_file.write(answer_text + '\n')
+                    span_file.write(str(answer_start) + ',' + str(answer_end) + '\n')
+                
+                context_file.write(context + '\n' + '-' + '\n')
+                question_file.write('-' + '\n')
+                answer_file.write('-' + '\n')
+                span_file.write('-' + '\n')
+
+get_data_test(squad_dataset)
